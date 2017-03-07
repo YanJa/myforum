b08d5a30beddb7beabfdcc97de87d2607732e4d8
yanja <molu_guest@126.com>
2017-03-08 00:09:40 +0800

    第六课第一节完毕 新建立block app 学习视图函数，模板和模型构成 将板块信息存入数据库 使用 django admin页面

--- forum/forum/views.py
+++ forum/forum/views.py
@@ -1,7 +1,12 @@
-from django.http import HttpResponse
 from django.shortcuts import render
+from block.models import Block
 
 
 def index(request):
-
-    return render(request, "index.html")
+    # block_info = [{"name": "Go专区", "desc": "go语言学习", "manager": "select"},
+    #               {"name": "Python专区", "desc": "python语言学习", "manager": "select1"},
+    #               {"name": "Docker专区", "desc": "docker容器学习", "manager": "select2"},
+    #               {"name": "Django专区", "desc": "django基础学习", "manager": "select3"},
+    #               ]
+    block_info = Block.objects.all().order_by("-id")
+    return render(request, "index.html", {"blocks": block_info})
