1a6cea9a9d1cdc25b6f70edce2d36af1cb6a1414
yanjia <yanjia@cashsoso.com>
2017-10-20 00:40:00 +0800

    第6课, 暂时没完成, 将数据写入到数据表中 用到了models模块 定义了类以生成数据表

--- forum/block/models.py
+++ forum/block/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Block(models.Model):
+    """
+    版块模型,存放版块数据
+    """
+    name = models.CharField("版块名称", max_length=100)
+    manger = models.CharField("管理员", max_length=50)
+    desc = models.TextField("版块描述", max_length=200)
--- forum/forum/views.py
+++ forum/forum/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
+from block.models import Block
 
 # Create your views here.
 
 def index(request):
-    return render(request, "index.html")
+    ##res = [
+    #        {"name": "pandas专区", "desc": "10分钟入门pandas", "manager": "admin"},
+    #        {"name": "numpy专区", "desc": "numpy学习理解", "manager": "admin"},
+    #        {"name": "django专区", "desc": "django中间件怎么玩", "manager": "view"},
+    #        {"name": "pytho", "desc": "python yield 怎样return返回值", "manager": "admin"}]
+    res = Block.objects.all().order_by("-id")
+
+    return render(request, "index.html", {"block_info": res})
