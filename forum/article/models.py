619ec36114bc04e73c87539bcceae1da1d2db192
yanjia <yanjia@cashsoso.com>
2017-11-02 00:32:18 +0800

    文章列表页面的表写完毕, 第七课

--- forum/article/models.py
+++ forum/article/models.py
@@ -9,8 +9,7 @@ class Article(models.Model):
     block = models.ForeignKey(Block, verbose_name='所属版块')
     title = models.CharField("文章标题", max_length=100)
     content = models.CharField("文章内容", max_length=10000)
-    status = models.IntegerField("状态",
-                                 choices=((0, "正常"), (1, "删除")))
+    status = models.IntegerField("状态", choices=((0, "正常"), (1, "删除")))
     create_timestamp = models.DateTimeField("创建时间", auto_now_add=True)
     last_update_timestamp = models.DateTimeField("最后更新时间", auto_now=True)
 
--- forum/article/views.py
+++ forum/article/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
+from .models import Article
+from block.models import Block
 
 # Create your views here.
 
 def article_list(request, block_id):
+    template = "article_list.html"
     block_id = int(block_id)
-    return render(request, "article/article_list.html")
+    bl = Block.objects.get(id=block_id)
+    # 指定版块对应的文章信息
+    article_obj = Article.objects.filter(block=bl, status=0).order_by('-id')
+    response = {
+        "bl": bl,
+        "articles": article_obj
+    }
+    return render(request, template, response)
