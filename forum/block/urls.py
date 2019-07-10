835a4d7536d53a44afc8110692b2c17d1ba1fc0e
“yanjia” <yanja99@163.com>
2019-07-10 23:51:10 +0800

    版块功能完善,未完成

--- forum/block/urls.py
+++ forum/block/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
-from .views import BLockInfo
+from .views import BLockList, BLockInfo
 
 urlpatterns = [
-    url('^block_list/$', BLockInfo.as_view(), name="block_list")
+    url('^block_list$', BLockList.as_view(), name="block_list"),
+    url('^block_info/(?P<block_id>\d+)$', BLockInfo.as_view(), name="block_info")
+
 ]
--- forum/block/views.py
+++ forum/block/views.py
@@ -4,6 +4,19 @@ from block.models import Block
 
 
 class BLockInfo(View):
+    template_name = "block/block_info.html"
+
+    def get(self, request, block_id):
+        print(block_id)
+        block_obj = Block.objects.filter(status=0).order_by('-id')
+        response = {"block_info": block_obj, "user": request.user}
+        return render(request, self.template_name, response)
+
+    def post(self, requset):
+        pass
+
+
+class BLockList(View):
     template_name = "block/block_list.html"
 
     def get(self, request):
